609b71cad72ec20b8d5b11ae235d447265961c18
Tiago <Tiago@DESKTOP-S15FL5L>
2025-04-20 01:32:00 +0100

    Add project files.

---
+++ ProductClientHubApi/Controllers/ClientesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectClientHub.Communication.Requests;
+using ProjectClientHub.Communication.Responses;
+
+namespace ProductClientHubApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseClientJson), StatusCodes.Status201Created)]
+        public IActionResult Register([FromBody] RequestClientJson request)
+        {
+            return Created();
+        }
+
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ProjectClientHub.Communication/Responses/ResponseClientJson.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectClientHub.Communication.Responses
+{
+    public class ResponseClientJson
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
